479f26d749ff9f1b4d037f97f90ed32dc9ba9c2b
JKozma81 <janos.kozma81@gmail.com>
2020-02-06 11:10:35 +0100

    postbox homework working

--- mailbox/controllers/messages_ctrl.js
+++ mailbox/controllers/messages_ctrl.js
@@ -1,55 +1,60 @@
 const fetch = require('node-fetch');
-const messages = require('../db/mail');
+const { Message, addMessage, getMessages } = require('../db/mail');
 
 const myUrl = process.argv[2];
 
 function getInbox(req, res) {
-    const inboxMsgs = messages.getMessages('inbox');
-    res.render('inbox', { inboxMsgs });
+	const inboxMsgs = getMessages('inbox');
+	res.render('inbox', { inboxMsgs });
 }
 
 function getSent(req, res) {
-    const sentMsgs = messages.getMessages('sent');
-    res.render('inbox', { sentMsgs });
+	const sentMsgs = getMessages('sent');
+	res.render('inbox', { sentMsgs });
 }
 
 function getCompose(req, res) {
-
-    if (Object.keys(req.query).length) {
-        const replyMsg = req.query;
-        res.render('compose', { replyMsg });
-        return;
-    }
-    res.render('compose');
+	if (Object.keys(req.query).length) {
+		const replyMsg = req.query;
+		res.render('compose', { replyMsg });
+		return;
+	}
+	res.render('compose');
 }
 
 function getDetails(req, res) {
-    const msg = messages.getMessages('inbox').find(msg => msg.id == req.params.msgID);
-    res.render('msgDetail', { msg });
+	const msg = getMessages('inbox').find((msg) => msg.id == req.params.msgID);
+	res.render('msgDetail', { msg });
 }
 
 function receiveMsg(req, res) {
-    messages.addMessage('inbox', req.body);
-    res.status(200).json('ok');
+	addMessage('inbox', req.body);
+	res.status(200).json('ok');
 }
 
 function sendMsg(req, res) {
-    const msg = new messages.Message(myUrl, req.body.to, req.body.subject, req.body.content);
-    const address = req.body.to;
+	const { to, subject, content } = req.body;
+	const msg = new Message(myUrl, to, subject, content);
+	const address = req.body.to;
 
-    fetch(`http://${address}/inbox`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(msg)
-    }).then(result => {
-        if (result.status === 200) {
-            res.redirect('/compose?alert=success');
-            return;
-        }
-        res.redirect('/compose?alert=failed');
-    }).catch(err => console.log(err))
+	fetch(`http://${address}/inbox`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(msg)
+	})
+		.then((result) => {
+			if (result.status === 200) {
+				res.redirect('/compose?alert=success');
+				return;
+			}
+			throw new Error("Can't send message...");
+		})
+		.catch((err) => {
+			console.log(err);
+			res.redirect('/compose?alert=failed');
+		});
 }
 
-module.exports = { getInbox, getSent, getCompose, getDetails, receiveMsg, sendMsg }
+module.exports = { getInbox, getSent, getCompose, getDetails, receiveMsg, sendMsg };
--- mailbox/db/mail.js
+++ mailbox/db/mail.js
@@ -1,42 +1,39 @@
-// const inboxMessages = [];
-const sentMessages = [];
-
 class Message {
-    constructor(messageFrom, messageTo, subject, content) {
-        this.id = Math.floor(Math.random() * 1000 + 1);
-        this.from = messageFrom;
-        this.to = messageTo;
-        this.subject = subject;
-        this.content = content;
+	constructor(messageFrom, messageTo, subject, content) {
+		this.id = Math.floor(Math.random() * 1000 + 1);
+		this.from = messageFrom;
+		this.to = messageTo;
+		this.subject = subject;
+		this.content = content;
 
-        const d = new Date();
-        const result = d.toISOString();
+		const d = new Date();
+		const result = d.toISOString();
 
-        this.date = result;
-    }
+		this.date = result;
+	}
 }
 
-const inboxMessages = [new Message('1a2b3c4d.ngrok.io', '4d3c2b1a.ngrok.io', 'Teszt', 'This is a test message')];
+const inboxMessages = [ new Message('1a2b3c4d.ngrok.io', '4d3c2b1a.ngrok.io', 'Teszt', 'This is a test message') ];
+const sentMessages = [];
 
 function getMessages(flag) {
-    switch (flag) {
-        case 'inbox':
-            return inboxMessages;
-        case 'sent':
-            return sentMessages;
-    }
+	switch (flag) {
+		case 'inbox':
+			return inboxMessages;
+		case 'sent':
+			return sentMessages;
+	}
 }
 
 function addMessage(flag, msg) {
-    switch (flag) {
-        case 'inbox':
-            inboxMessages.push(msg);
-            break;
-        case 'sent':
-            sentMessages.push(msg);
-            break;
-    }
+	switch (flag) {
+		case 'inbox':
+			inboxMessages.push(msg);
+			break;
+		case 'sent':
+			sentMessages.push(msg);
+			break;
+	}
 }
 
-
-module.exports = { Message, addMessage, getMessages }
+module.exports = { Message, addMessage, getMessages };
