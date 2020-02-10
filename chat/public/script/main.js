0a2e19bdf33e0b8ae18b037543b7e4b00973dd79
Kozma János <41972874+JKozma81@users.noreply.github.com>
2020-02-10 23:07:26 +0100

    Chat app finished

--- chat/public/script/main.js
+++ chat/public/script/main.js
@@ -2,8 +2,12 @@ const messageForm = document.querySelector('.message-form');
 const chatDisplay = document.querySelector('.chat-text');
 const messageText = document.querySelector('input[name="content"]');
 const refreshButton = document.querySelector('.refresh');
+const modal = document.querySelector('.modal');
+const errorText = document.querySelector('.error-message');
+const formForServerUrl = document.querySelector('.modal form');
+const urlInput = document.querySelector('#server_url');
 
-const serverUrl = 'http://localhost:3000'
+let serverUrl = '';
 
 function getMessages() {
     return fetch(serverUrl + '/messages');
@@ -29,11 +33,26 @@ function displayMessages(messages, display) {
     display.innerHTML = chatText;
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    const data = getMessages();
-    data.then(result => result.json())
+formForServerUrl.addEventListener('submit', (e) => {
+    e.preventDefault();
+    serverUrl = 'http://' + urlInput.value;
+    fetch(`${serverUrl}/messages`)
+        .then(result => {
+            if (result.status === 404) {
+                throw new Error();
+            }
+            urlInput.value = '';
+            return result.json();
+        })
         .then(messageData => {
+            modal.style.display = 'none';
+            errorText.style.display = 'none';
             displayMessages(messageData, chatDisplay);
+        })
+        .catch(err => {
+            console.error(err.toString());
+            errorText.textContent = 'Content not found, please provide a new server address!';
+            errorText.style.display = 'block';
         });
 });
 
