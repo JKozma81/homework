ecf9c92a3a28b4617368a8636ca15c5250e34056
Kozma János <41972874+JKozma81@users.noreply.github.com>
2020-02-17 17:25:46 +0100

    quick fix

--- new_postbox/postboxsrv.js
+++ new_postbox/postboxsrv.js
@@ -10,9 +10,7 @@ const PORT = 3000;
 
 const app = express();
 
-app.engine('handlebars', hbs({
-    helpers: require('./config/handlebars-helper')
-}));
+app.engine('handlebars', hbs());
 app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
