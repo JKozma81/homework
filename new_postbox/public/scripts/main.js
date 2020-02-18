84b47bf638397e872826241b3d08d40c3d8e50a5
JKozma81 <janos.kozma81@gmail.com>
2020-02-18 10:09:01 +0100

    new solution

--- new_postbox/public/scripts/main.js
+++ new_postbox/public/scripts/main.js
@@ -7,49 +7,76 @@ const alertFail = document.querySelector('.alert-danger');
 const url = 'http://localhost:3000';
 
 function getMessages(url) {
-    fetch(`${url}/inbox`)
-        .then(stream => stream.json())
-        .then(data => {
-            console.log(data);
-        })
+	fetch(`${url}/inbox`).then((stream) => stream.json()).then((data) => {
+		console.log(data);
+	});
 }
 
-window.addEventListener('load', function () {
-    const form = document.querySelector('.needs-validation');
-    form.addEventListener('submit', (event) => {
-        event.preventDefault();
-        if (form.checkValidity() === false) {
-            event.stopPropagation();
-        }
+// needs refactoring
+function createAlert(flag, message) {
+	const alertBox = document.querySelector('.alert-container');
+	const div = document.createElement('DIV');
+	div.classList.add('alert', flag === 'Success' ? 'alert-success' : 'alert-danger', 'fade', 'show');
+	div.setAttribute('role', 'alert');
+	const strng = document.createElement('STRONG');
+	strng.textContent = `${flag}!`;
+	div.appendChild(strng);
+	const text = document.createTextNode(` ${message}`);
+	div.appendChild(text);
+	const btn = document.createElement('BUTTON');
+	btn.setAttribute('type', 'button');
+	btn.classList.add('close');
+	btn.setAttribute('data-dismiss', 'alert');
+	btn.setAttribute('aria-label', 'Close');
+	div.appendChild(btn);
+	const span = document.createElement('SPAN');
+	span.setAttribute('aria-hidden', 'true');
+	span.textContent = '\327';
+	btn.appendChild(span);
+	alertBox.prepend(div);
+}
 
-        form.classList.add('was-validated');
+window.addEventListener(
+	'load',
+	function() {
+		const form = document.querySelector('.needs-validation');
+		form.addEventListener('submit', (event) => {
+			event.preventDefault();
+			if (form.checkValidity() === false) {
+				event.stopPropagation();
+			}
 
-        fetch(`${url}/outgoing`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'Application/json'
-            },
-            body: JSON.stringify({ nameAndAddress: nameAndAddress.value, message: messageText.value })
-        })
-            .then(resp => {
-                form.classList.remove('was-validated')
-                if (resp.status !== 200) {
-                    alertFail.classList.add('show');
-                    setTimeout(() => alertFail.classList.remove('show'), 3000)
-                    return;
-                }
-                setTimeout(() => alertSuccess.classList.remove('show'), 3000)
-                alertSuccess.classList.add('show');
-                nameAndAddress.value = '';
-                messageText.value = '';
+			form.classList.add('was-validated');
 
-            })
-            .catch(err => console.error(err.toString()));
-    })
-    getMessages(url);
-}, false);
+			fetch(`${url}/outgoing`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'Application/json'
+				},
+				body: JSON.stringify({ nameAndAddress: nameAndAddress.value, message: messageText.value })
+			})
+				.then((resp) => resp.json())
+				.then((resp) => {
+					form.classList.remove('was-validated');
+					if (resp.status !== 200) {
+						setTimeout(() => alertFail.classList.remove('show'), 3000);
+						alertFail.classList.add('show');
+						console.log(resp.message);
+						return;
+					}
+					setTimeout(() => alertSuccess.classList.remove('show'), 3000);
+					alertSuccess.classList.add('show');
+					nameAndAddress.value = '';
+					messageText.value = '';
 
-
+					createAlert('Success', 'Sikerült');
+				})
+				.catch((err) => console.error(err.toString()));
+		});
+		getMessages(url);
+	},
+	false
+);
 
 /**
  *  Message collapse
